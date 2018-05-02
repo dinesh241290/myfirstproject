78a0f7613e7b5d05cc58a434a250a00fb0b5c01a
Dinesh <dinesh.webapi@g.com>
2018-05-02 17:01:03 -0400

    This is my First application

---
+++ MVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCApplication.Models;
+
+namespace MVCApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult EmployeList()
+        {
+            Employee emp = new Employee()
+            {
+                employeeID = 1,
+                employeeName = "Dinesh",
+                Address = "Hyderabad",
+        };
+            return View(emp);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVCApplication/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCApplication.Models
+{
+    public class Employee
+    {
+        [Required]
+        public int employeeID { get; set; }
+        public string employeeName { get; set; }
+        public string Address { get; set; }
+    }
+}
